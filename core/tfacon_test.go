92e2c9d88dc58cd81c96a0928a1fe41e501fc6fd
JunqiZhang0 <junqzhan@redhat.com>
2021-10-05 00:19:41 -0400

    updated one test file

--- core/tfacon_test.go
+++ core/tfacon_test.go
@@ -10,45 +10,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// var viperconfigfortest *viper.Viper = viper.New()
-
-// func Test_runHelper(t *testing.T) {
-// 	viperconfigfortest.SetConfigType("ini")
-// 	viperconfigfortest.SetDefault("config.concurrency", true)
-// 	viperconfigfortest.SetDefault("config.retry_times", 1)
-// 	viperconfigfortest.SetDefault("config.add_attributes", false)
-// 	type args struct {
-// 		viperConfig *viper.Viper
-// 		ids         []string
-// 		con         TFACon
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 	}{
-// 		// TODO: Add test cases.
-// 		{
-// 			name: "test with single id in ids",
-// 			args: args{
-// 				viperConfig: viperconfigfortest,
-// 				ids:         []string{"12306"},
-// 				con:         GetCon(vipertfaconfortest),
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			defer func() {
-// 				if r := recover(); r != nil {
-// 					t.Fatalf("%s failed on: %s\n", tt.name, r)
-// 				}
-// 			}()
-// 			runHelper(tt.args.viperConfig, tt.args.ids, tt.args.con)
-
-// 		})
-// 	}
-// }
-
 var vipertfaconfortest *viper.Viper = viper.New()
 
 func TestGetCon(t *testing.T) {
